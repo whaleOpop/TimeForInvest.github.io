43c0cd94a9f00eddb73b1ba647f3cf3a498427eb
WhaleOpop <al-agabani-kit@yandex.ru>
2023-06-12 05:06:43 +0300

    Test fix sender mailer ezzzz

--- mail.php
+++ mail.php
@@ -12,14 +12,14 @@ $problems = $_POST['user_problems']
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'firpaupfur@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '77322850nN'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'invest.vremya@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'RtR-yrX-s6b-YsB'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('firpaupfur@gmail.com'); // от кого будет уходить письмо?
+$mail->setFrom('invest.vremya@mail.ru'); // от кого будет уходить письмо?
 $mail->addAddress('al-agabani-kit@yandex.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
